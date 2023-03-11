e149b4a1502b78c2e086313bc7b46763da3fb8c2
Exray-bandit001 <100499722+JOSHUANDISHU@users.noreply.github.com>
2023-03-11 14:28:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-ALIVE_MSG: process.env.ALIVE_MSG ==='default' : process.env.ALIVE_MSG,
-LANG: process.env.LANG ==='EN' : process.env.LANG,
+ALIVE_MSG: process.env.ALIVE_MSG === 'default' : process.env.ALIVE_MSG,
+LANG: process.env.LANG === 'EN' : process.env.LANG,
 ALIVE_LOGO: process.env.ALIVE_LOGO === 'https://i.imgur.com/v8iuP8v.jpg' : process.env.ALIVE_LOGO,
-FOOTER: process.env.FOOTER ==='ᴀꜱᴛʀᴏ-ᴍᴅ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
-CAPTION: process.env.CAPTION ==='*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀꜱᴛʀᴏ-ᴍᴅ*' : process.env.CAPTION,
-ONLY_GROUP: process.env.ONLY_GROUP ==='false' : process.env.ONLY_GROUP,
-INBOX_BL_MSG: process.env.INBOX_BL_MSG ==='*Inbox not allowed' : process.env.INBOX_BL_MSG,
-HOST: process.env.HOST === '-' : process.env.HOST,
+FOOTER: process.env.FOOTER === 'ᴀꜱᴛʀᴏ-ᴍᴅ ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
+CAPTION: process.env.CAPTION === '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀꜱᴛʀᴏ-ᴍᴅ*' : process.env.CAPTION,
+ONLY_GROUP: process.env.ONLY_GROUP === 'false' : process.env.ONLY_GROUP,
+INBOX_BL_MSG: process.env.INBOX_BL_MSG === '*Inbox not allowed' : process.env.INBOX_BL_MSG,
+HOST: process.env.HOST === '.' : process.env.HOST,
 SESSION_ID: process.env.SESSION_ID === 'ASTRO=QigCQJBY#sjWtQ37Ua-1NpaV5qD7X6YR7z6SGdAAvZL5hKYNtQxM' : process.env.SESSION_ID
 };
